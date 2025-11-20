d81a27e5b7d6ff32c254aea780e0a7680796e084
Falk Döring <falk.doering@fadoe.de>
2025-11-20 17:33:39 +0100

    run editorconfig on current loaded file

--- main.ts
+++ main.ts
@@ -1,134 +1,117 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, TFile } from "obsidian";
+import * as editorconfig from "editorconfig";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class EditorConfigFormatter extends Plugin {
 	async onload() {
-		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.registerEvent(
+			this.app.vault.on("modify", async (file) => {
+				if (file instanceof TFile && file.extension === "md") {
+					await this.applyEditorConfig(file);
+				}
+			})
+		);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, _view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: "format-md-editorconfig",
+			name: "Editorconfig: Formate file with editorconfig",
 			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
+				const file = this.app.workspace.getActiveFile();
+				if (file && file.extension === "md") {
 					if (!checking) {
-						new SampleModal(this.app).open();
+						this.applyEditorConfig(file);
 					}
 
-					// This command will only show up in Command Palette when the check function returns true
 					return true;
 				}
+
+				return false;
+			},
+		});
+	}
+
+	async applyEditorConfig(file: TFile) {
+		const vaultPath = this.app.vault.adapter.getBasePath();
+		const filePath = `${vaultPath}/${file.path}`;
+		const config = await editorconfig.parse(filePath);
+
+		const content = await this.app.vault.read(file);
+		const lines = content.split(/\r?\n/);
+
+		let inCodeBlock = false;
+		let inFrontmatter = false;
+		const newLines: string[] = [];
+
+		for (let i = 0; i < lines.length; i++) {
+			let line = lines[i];
+
+			// Detect frontmatter block
+			if (i === 0 && line.trim() === "---") {
+				inFrontmatter = true;
+				newLines.push(line);
+				continue;
 			}
-		});
+			if (inFrontmatter) {
+				newLines.push(line);
+				if (line.trim() === "---") inFrontmatter = false;
+				continue;
+			}
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+			// Detect code block
+			if (line.trim().startsWith("```")) {
+				inCodeBlock = !inCodeBlock;
+				newLines.push(line);
+				continue;
+			}
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+			if (inCodeBlock) {
+				newLines.push(line);
+				continue;
+			}
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+			// Process normal Markdown text
+			const leadingWhitespaceMatch = line.match(/^[ \t]*/);
+			const leading = leadingWhitespaceMatch ? leadingWhitespaceMatch[0] : "";
+			const contentPart = line.slice(leading.length);
 
-	onunload() {
+			// Calculate indentation level for nested lists
+			let level = 0;
+			for (const char of leading) {
+				if (char === "\t") level += 1;
+				else if (char === " ") level += 1 / (config.indent_size || 2);
+			}
 
-	}
+			// Apply new indentation based on .editorconfig settings
+			let newLeading = "";
+			if (config.indent_style === "tab") {
+				newLeading = "\t".repeat(Math.round(level));
+			} else {
+				newLeading = " ".repeat(Math.round(level) * (config.indent_size || 2));
+			}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+			line = newLeading + contentPart;
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+			// Remove trailing whitespace if configured
+			if (config.trim_trailing_whitespace) {
+				line = line.replace(/[ \t]+$/g, "");
+			}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+			newLines.push(line);
+		}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+		let newContent = newLines.join(
+			config.end_of_line === "crlf" ? "\r\n" : "\n"
+		);
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+		if (config.insert_final_newline) {
+			if (!newContent.endsWith("\n") && !newContent.endsWith("\r\n")) {
+				newContent += config.end_of_line === "crlf" ? "\r\n" : "\n";
+			}
+		}
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		if (newContent !== content) {
+			await this.app.vault.modify(file, newContent);
+			console.log(`.editorconfig used on ${file.path}`);
+		}
 	}
 }
